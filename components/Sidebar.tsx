d70ddbebe5bce8d3c0e6abe2426207dd4924f288
Shreyas Jain <shrys.jain@gmail.com>
2024-07-16 16:06:59 -0400

    setup + routing + sidebar

---
+++ components/Sidebar.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { sidebarRoutes } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const Sidebar = () => {
+  const pathname = usePathname();
+  return (
+    <section className="sticky left-0  top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+      <div className="flex flex-1 flex-col gap-6">
+        {sidebarRoutes.map((route) => {
+          const isActive =
+            pathname === route.route || pathname.startsWith(route.route);
+
+          return (
+            <Link
+              href={route.route}
+              key={route.label}
+              className={cn(
+                "flex gap-4 items-center p-4 rounded-lg justify-start",
+                {
+                  "bg-blue-1": isActive,
+                }
+              )}
+            >
+              <Image
+                src={route.imgUrl}
+                alt={route.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-lg font-semibold max-lg:hidden">
+                {route.label}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Sidebar;
