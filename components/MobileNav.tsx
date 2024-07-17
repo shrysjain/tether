8353df2e563b8d26f002fe033283fd4207efee6a
Shreyas Jain <shrys.jain@gmail.com>
2024-07-17 00:36:33 -0400

    implement authentication + responsiveness

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,34 +1,77 @@
+"use client";
+
 import React from "react";
 import {
   Sheet,
+  SheetClose,
   SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import Image from "next/image";
+import Link from "next/link";
+import { sidebarRoutes } from "@/constants";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const MobileNav = () => {
+  const pathname = usePathname();
+
   return (
     <section className="w-full max-w-[264px]">
       <Sheet>
-        <SheetTrigger>
+        <SheetTrigger asChild>
           <Image
             src="/icons/hamburger.svg"
             width={36}
             height={36}
-            alt="Open navbar"
+            alt="Hamburger icon"
+            className="cursor-pointer sm:hidden"
           />
         </SheetTrigger>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
+        <SheetContent side="left" className="border-none bg-dark-1">
+          <Link href="/" className="flex items-center gap-1">
+            <Image
+              src="/icons/logo.svg"
+              width={32}
+              height={32}
+              alt="Tether Logo"
+              className="max-sm:size-10"
+            />
+            <p className="text-[26px] font-extrabold text-white">Tether</p>
+          </Link>
+
+          <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
+            <SheetClose asChild>
+              <section className="flex h-full flex-col gap-6 pt-16 text-white">
+                {sidebarRoutes.map((route) => {
+                  const isActive = pathname === route.route;
+
+                  return (
+                    <SheetClose asChild key={route.route}>
+                      <Link
+                        href={route.route}
+                        key={route.label}
+                        className={cn(
+                          "flex gap-4 items-center p-4 rounded-lg w-full max-w-60",
+                          {
+                            "bg-blue-1": isActive,
+                          }
+                        )}
+                      >
+                        <Image
+                          src={route.imgUrl}
+                          alt={route.label}
+                          width={20}
+                          height={20}
+                        />
+                        <p className="font-semibold">{route.label}</p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+              </section>
+            </SheetClose>
+          </div>
         </SheetContent>
       </Sheet>
     </section>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,11 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import MobileNav from "./MobileNav";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+    <nav className="flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/icons/logo.svg"
@@ -20,7 +21,10 @@ const Navbar = () => {
       </Link>
 
       <div className="flex-between gap-5">
-        {/* Clerk */}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+
         <MobileNav />
       </div>
     </nav>
